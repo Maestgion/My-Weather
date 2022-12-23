104fa917a23580e825d4873bd94e2aaf0974d41d
Abhishek Dubey <7abhishek1410@gmail.com>
2022-12-23 16:48:13 +0530

    current-widget-ui-done

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -1,27 +1,30 @@
 import React from 'react'
+import cloudIcon from "../../Assets/Icons/04d.png"
 
 const CurrentWeather = () => {
   return (
     <>
-       <div className='grid grid-cols-2 p-8 w-[30vw] h-[15vw] rounded-lg bg-zinc-700 text-white'>
+       <div className='grid grid-cols-2 p-8 w-[34vw] rounded-lg bg-zinc-700 text-white gap-10'>
 
           <div className='flex flex-col justify-between'>
               <div className='leading-none'>
-                <p className='font-bold text-xl'>Belgrade, RS</p>
-                <p>broken,clouds</p>
+                <p className='font-bold text-3xl'>Belgrade, RS</p>
+                <p className='text-lg tracking-wider'>broken,clouds</p>
               </div>
 
 
             <div>
-              <p className='text-7xl font-bold'>22°C</p>
+              <p className='text-8xl font-bold'>22°C</p>
             </div>
 
           </div>
 
           <div className='flex flex-col justify-between leading-tight'>
             
-            <div></div>
-            <div className='flex justify-between '>
+            <div className='relative left-[5vw] bottom-[3vh] ' >
+              <img src={cloudIcon} alt={cloudIcon} width={100}   />
+            </div>
+            <div className='flex justify-between'>
               <div > 
                 <p >Details</p>
                 <p>Feels like</p>
@@ -31,7 +34,7 @@ const CurrentWeather = () => {
               </div>
 
 
-              <div>
+              <div >
                 <p className='invisible'>jjvhchgc</p>
                 <p>22°C</p>
                 <p>2.06 m/s</p>
